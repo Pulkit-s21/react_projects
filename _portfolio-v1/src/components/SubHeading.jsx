580850dc77bdc2f071c419b0a6b31e4abd5a9dff
Pulkit Saxena <pulkitsaxena795@gmail.com>
2023-09-07 09:51:47 +0530

    "RoughNotation" "Circle Added as of now"

--- _portfolio-v1/src/components/SubHeading.jsx
+++ _portfolio-v1/src/components/SubHeading.jsx
@@ -1,17 +1,29 @@
+import { RoughNotation } from "react-rough-notation"
+
 export const SubHeading = () => {
   return (
     <div className="flex flex-col items-center gap-8">
       <h1 className="text-2xl md:text-3xl lg:text-5xl text-yellow-500">
         Hi there, My Name is{" "}
-        <span className="font-Cabin block">Pulkit Saxena</span>
+        <RoughNotation type="circle" color="white" order={1} className="font-Cabin block">Pulkit Saxena</RoughNotation>
       </h1>
-      <p className="text-lg md:text-xl lg:text-2xl max-w-lg lg:max-w-2xl tracking-wider">
-        <span className="text-3xl md:text-5xl lg:text-7xl font-Cabin block">
+      <div className="text-lg md:text-xl lg:text-2xl max-w-lg lg:max-w-2xl tracking-wider">
+        <RoughNotation
+          RoughNotation
+          type="underline"
+          strokeWidth={5}
+          show={true}
+          order={2}
+          animate={true}
+          animationDuration={1000}
+          color="red"
+          className="text-3xl md:text-5xl lg:text-7xl font-Cabin block"
+        >
           Front-End Developer
-        </span>{" "}
+        </RoughNotation>{" "}
         with the curiousity to explore and learn loads of new things and keep on
         progressing.
-      </p>
+      </div>
     </div>
   )
 }
