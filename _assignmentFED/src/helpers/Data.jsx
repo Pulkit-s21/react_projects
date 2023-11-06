fd03e5156d7514c5571abac608a88e1e33e252dc
Pulkit Saxena <pulkitsaxena795@gmail.com>
2023-11-06 17:45:58 +0530

    "Medical Invoice" "Map & Filter" "Happy" "On own"

---
+++ _assignmentFED/src/helpers/Data.jsx
@@ -0,0 +1,12 @@
+export const Data = [
+  { name: "Norflox-TZ Tablet", price: 9.94, quantity: 100 },
+  { name: "Paracetamol-650mg", price: 0.95, quantity: 60 },
+  { name: "PAN-D", price: 14, quantity: 80 },
+  { name: "Feboxa-40mg", price: 14.3, quantity: 95 },
+  { name: "Torleva XR", price: 21.6, quantity: 15 },
+  { name: "Torthrocin", price: 26, quantity: 177 },
+  { name: "Dolo-650", price: 2.24, quantity: 67 },
+  { name: "Medrol", price: 10.8, quantity: 93 },
+  { name: "Eritel", price: 3.9, quantity: 53 },
+  { name: "Naprosyn", price: 8.7, quantity: 133 },
+]
